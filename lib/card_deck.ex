6feafe0eaf2bd4ad620bcd2c56031f1b16cf2781
Daniel Kukula <daniel.kuku@gmail.com>
2020-06-23 20:20:44 +0100

    drop to pile function

--- lib/card_deck.ex
+++ lib/card_deck.ex
@@ -59,19 +59,33 @@ defmodule CardDeck do
   returns numeric value of a card
 
   # Examples
-    iex> CardDeck.value("T") < CardDeck.value("K")
+    iex> CardDeck.rank_value("T") < CardDeck.rank_value("K")
     true
-    iex> CardDeck.value({"T", "C"})
+    iex> CardDeck.rank_value({"T", "C"})
     10
   """
-  @spec value(card | String.t()) :: integer
-  def value("A"), do: 14
-  def value("T"), do: 10
-  def value("J"), do: 11
-  def value("Q"), do: 12
-  def value("K"), do: 13
-  def value({rank, _suit}), do: value(rank)
-  def value(number_value), do: String.to_integer(number_value)
+  @spec rank_value(card | String.t()) :: integer
+  def rank_value("A"), do: 14
+  def rank_value("T"), do: 10
+  def rank_value("J"), do: 11
+  def rank_value("Q"), do: 12
+  def rank_value("K"), do: 13
+  def rank_value({rank, _suit}), do: rank_value(rank)
+  def rank_value(number) when is_integer(number), do: number
+  def rank_value(number) when is_binary(number), do: String.to_integer(number)
+  def rank_value(number), do: IO.inspect(number)
+
+  @doc """
+  returns card with numeric rank
+
+  # Examples
+    iex> CardDeck.to_value({"A", "D"})
+    {14, "D"}
+    iex> CardDeck.to_value({"T", "C"})
+    {10, "C"}
+  """
+  @spec to_value(card | String.t()) :: {integer(), suit}
+  def to_value({r, s}), do: {rank_value(r), s}
 
   @doc """
   Returns rank of card
@@ -100,7 +114,7 @@ defmodule CardDeck do
   """
   @spec sort_by_rank(deck) :: deck
   def sort_by_rank(deck) do
-    Enum.sort(deck, &(value(&1) <= value(&2)))
+    Enum.sort(deck, &(rank_value(&1) <= rank_value(&2)))
   end
 
   @doc """
@@ -126,7 +140,7 @@ defmodule CardDeck do
 
   """
   @spec sort(deck) :: deck
-  def sort(deck), do: deck |> sort_by_rank() |> sort_by_suit()
+  def sort(deck), do: deck |> sort_by_suit() |> sort_by_rank()
 
   @doc """
   Deals amount of card from deck
@@ -165,5 +179,27 @@ defmodule CardDeck do
     end)
     |> Enum.unzip()
     |> elem(0)
+  end
+
+  @doc """
+   Drops cards at selected indexes to pile and returns pile and deck/hand
+
+   # Examples
+   [{"2", "C"}, {"2", "D"}, {"2", "H"}, {"2", "S"}] |> CardDeck.drop_to_pile([0,3])
+   {[{"2", "C"}, {"2", "S"}], [{"2", "D"}, {"2", "H"}]}
+  """
+  @spec drop_to_pile(deck, [integer]) :: {deck, deck}
+  def drop_to_pile(deck, positions_list) do
+    [pile, deck] =
+      deck
+      |> Enum.with_index()
+      |> Enum.group_by(fn {_c, i} ->
+        !Enum.member?(positions_list, i)
+      end)
+      |> Enum.map(fn {_pile_or_deck, deck} ->
+        deck |> Enum.unzip() |> elem(0)
+      end)
+
+    {pile, deck}
   end
 end
